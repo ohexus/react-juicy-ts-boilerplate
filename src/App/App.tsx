5fb2e1b4c92acf0a6932c821c1e806679b08534c
ohexus <moskalenko.taras.d@gmail.com>
2021-08-12 13:00:42 +0300

    created basic App component

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import styles from './App.scss';
+
+const App: FC = () => (
+  <div className={styles.app}>
+    <h1>App works!</h1>
+  </div>
+);
+
+export default App;
