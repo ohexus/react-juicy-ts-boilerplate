34316067ff578937d9fa834ec8ed4f92b6e4ef26
ohexus <moskalenko.taras.d@gmail.com>
2021-08-14 23:24:29 +0300

    created setup script

---
+++ internals/setup.ts
@@ -0,0 +1,59 @@
+import { askForNewProjectName } from './questions';
+import {
+  addAllToGitRepo,
+  cleanRepo,
+  createInitialCommit,
+  initGitRepo,
+  removeInternalDependencies,
+  removeInternalsDir,
+  updateIndexHtml,
+  updatePackageJson,
+  updateReadme,
+  updateTsConfig,
+} from './scripts';
+import { chalkColored, juicySpinner, printBasicMessage, printMessage } from './utils';
+
+const MESSAGES = {
+  FINAL: 'Project initialized.',
+  SCRIPTS_OVERVIEW: `
+Now you can run these commands:
+  ${chalkColored('yarn start', 'Cyan')}
+    Starts the development server.
+    Open ${chalkColored('http://localhost:9000', 'Cyan')} to view it in the browser.
+
+  ${chalkColored('yarn test', 'Cyan')}
+    Starts the tests.
+
+  ${chalkColored('yarn build', 'Cyan')}
+    Bundles the app into static files for production.
+
+  ${chalkColored('yarn lint', 'Cyan')}
+    Lints the app and shows errors and warnings in console.
+`,
+};
+
+export default async function setup(): Promise<void> {
+  const name = await askForNewProjectName();
+
+  juicySpinner.start();
+
+  await Promise.all([
+    updateIndexHtml(name),
+    updatePackageJson(name),
+    updateTsConfig(),
+    updateReadme(name),
+  ]);
+
+  await removeInternalDependencies();
+  await removeInternalsDir();
+
+  await cleanRepo();
+  await initGitRepo();
+  await addAllToGitRepo();
+  await createInitialCommit();
+
+  juicySpinner.stop();
+
+  printMessage(MESSAGES.FINAL);
+  printBasicMessage(MESSAGES.SCRIPTS_OVERVIEW);
+}
