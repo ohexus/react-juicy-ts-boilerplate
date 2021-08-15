17c8d0953c214c59196a4f464bba52a0ac4721d0
ohexus <moskalenko.taras.d@gmail.com>
2021-08-15 19:20:27 +0300

    extended QuestionMessages

--- internals/questions/QuestionMessages.ts
+++ internals/questions/QuestionMessages.ts
@@ -1,4 +1,7 @@
 const QUESTION_MESSAGES = {
+  GIT: {
+    NEW_ORIGIN: 'Please type the new git origin (or leave empty string)',
+  },
   PROJECT: {
     INVALID_NAME: 'Please enter a valid name.',
     NEW_NAME: 'Please type the new project name, e.g. HoneyProject',
