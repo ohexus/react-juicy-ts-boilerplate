04ad73670f5efd9672aada3a40b60652f95ffdc3
ohexus <moskalenko.taras.d@gmail.com>
2021-08-17 13:55:58 +0300

    updated project name question message

--- internals/questions/QuestionMessages.ts
+++ internals/questions/QuestionMessages.ts
@@ -4,7 +4,7 @@ const QUESTION_MESSAGES = {
   },
   PROJECT: {
     INVALID_NAME: 'Please enter a valid name.',
-    NEW_NAME: 'Please type the new project name, e.g. HoneyProject',
+    NEW_NAME: 'Please type the new project name, e.g. honey-project',
   },
 };
 
